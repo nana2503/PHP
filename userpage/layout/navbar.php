64b7ec21acacd48024aef9ca01be0e6991131153
Ngoc Na <buina5555@gmail.com>
2023-11-26 21:43:40 +0700

    done crud admin page, userpage, still missing cart, update later

--- userpage/layout/navbar.php
+++ userpage/layout/navbar.php
@@ -1,7 +1,10 @@
+<?php 
+include("C:/wamp64/www/DOANPHP/userpage/utlils/connectDB.php");
+?>
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-        <img src="./img/logo.webp" style="width:130px; margin-top: 2px;">
-        <li class="nav-item nav-link"></li>
+        <img src="../img/logo.webp" style="width:130px; margin-top: 2px;">
+        <li class="nav-item nav-link" style="padding: 0 80px;"></li>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -11,40 +14,27 @@
                     <a class="menu" aria-current="page" href="TrangChu.php">TRANG CHỦ</a>
                 </li>
                 <li class="nav-item dropdown nav-link">
-                    <a class="menu dropdown-toggle" href="BanhKem.html" data-bs-toggle="dropdown">SẢN PHẨM</a>
-                    <ul class="menu2 dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="menu2 mx-4" href="BanhKem.html">Bánh Kem Tươi</a></li>
-                        <li><a class="menu2 mx-4" href="#">Bánh Quy</a></li>
-                        <li><a class="menu2 mx-4" href="#">Bánh Piza</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item nav-link">
-                    <a class="menu" href="GioiThieu.html">GIỚI THIỆU</a>
+                    <a class="menu" href="DanhMuc.php">SẢN PHẨM</a>
                 </li>
                 <li class="nav-item nav-link">
                     <a class="menu" href="GioHang.html">GIỎ HÀNG</a>
                 </li>
                 <li class="nav-item nav-link">
-                    <?php
+                    <a class="menu" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php
                     session_start();
                     if (isset($_SESSION['name'])) {
                         echo "Xin chào, " . $_SESSION['name'] . "! 
                         <br>
-                        <li class='nav-item nav-link'>                                              
-                            <a class='menu' href='../../DOANPHP/adminpage/ElaAdmin/controller/LogoutController.php'>ĐĂNG XUẤT</a>
+                        <li class='nav-item nav-link' >                                              
+                            <a class='menu' href='../../../DOANPHP/adminpage/ElaAdmin/controller/LogoutController.php'>ĐĂNG XUẤT</a>
+                        </div>
                         </li>";
                     } else {
-                        echo "<a class='menu' href='../../DOANPHP/adminpage/ElaAdmin/view/login.php'>ĐĂNG NHẬP | ĐĂNG KÝ</a>";
+                        echo "<a class='menu' href='../../../DOANPHP/adminpage/ElaAdmin/view/login.php'>ĐĂNG NHẬP | ĐĂNG KÝ</a>";
                     }
-                    ?>
+                    ?></a>
                 </li>
             </ul>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Tìm kiếm sản phẩm" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">
-                    <i class="bi bi-search"></i>
-                </button>
-            </form>
         </div>
     </div>
 </nav>
--- userpage/view/TrangChu.php
+++ userpage/view/TrangChu.php
@@ -6,29 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trang chủ - Freshgarden</title>
-    <link rel="shortcut icon" href="./img/logo.webp" type="image/png" />
+    <link rel="shortcut icon" href="../img/logo.webp" type="image/png" />
     <link href='//theme.hstatic.net/200000411281/1000827595/14/custom-styles.scss.css?v=176' rel='stylesheet' type='text/css' media='all' />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 </head>
 <hrad>
-    <link rel="stylesheet" type="text/css" href="TrangChu.css">
+    <link rel="stylesheet" type="text/css" href="assets/TrangChu.css">
 </hrad>
 
 <body>
-    <?php include("./layout/navbar.php") ?>
+    <?php include("../layout/navbar.php") ?>
 
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="img/caroseo1.1.webp" class="d-block w-100" alt="...">
+                <img src="../img/caroseo1.1.webp" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="img/caroseo2.1.webp" class="d-block w-100" alt="...">
+                <img src="../img/caroseo2.1.webp" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="img/caroseo3.1.webp" class="d-block w-100" alt="...">
+                <img src="../img/caroseo3.1.webp" class="d-block w-100" alt="...">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -44,7 +44,7 @@
     <section class="home home-featured-product">
         <div class="section-heading text-center style-dark">
             <h2 class="section-title">
-                <div class="icon"><img src="./img/imgSPNB.webp" alt="icon"></div>
+                <div class="icon"><img src="../img/imgSPNB.webp" alt="icon"></div>
                 <span style="font-size: 45px; color: #fff;">Sản phẩm nổi bật</span>
             </h2>
             <div class="container" style="font-size: 17px;  color: #fff;">Cập nhật về những sản phẩm nổi bật nhất từ Fresh Garden</div>
@@ -53,7 +53,7 @@
             <div class="row">
                 <div class="col-lg-3 col-md-3 my-5 col-sm-6">
                     <a href="">
-                        <img src="./img/spnb1.webp" class="spnb dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
+                        <img src="../img/spnb1.webp" class="spnb dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
                     </a>
                     <div class="text-center">
                         <h5><a href="">BÁNH KEM RED VELVET</a></h5>
@@ -62,7 +62,7 @@
                 </div>
                 <div class="col-lg-3 col-md-3 my-5 col-sm-6">
                     <a href="">
-                        <img src="./img/spnb2.webp" class="spnb dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
+                        <img src="../img/spnb2.webp" class="spnb dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
                     </a>
                     <div class="text-center">
                         <h5><a href="">BÁNH KEM RED VELVET</a></h5>
@@ -71,7 +71,7 @@
                 </div>
                 <div class="col-lg-3 col-md-3 my-5 col-sm-6">
                     <a href="">
-                        <img src="./img/spnb3.webp" class="spnb dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
+                        <img src="../img/spnb3.webp" class="spnb dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
                     </a>
                     <div class="text-center">
                         <h5><a href="">BÁNH KEM RED VELVET</a></h5>
@@ -80,7 +80,7 @@
                 </div>
                 <div class="col-lg-3 col-md-3 my-5 col-sm-6">
                     <a href="">
-                        <img src="./img/spnb4.webp" class="spnb dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
+                        <img src="../img/spnb4.webp" class="spnb dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
                     </a>
                     <div class="text-center">
                         <h5><a href="">BÁNH KEM RED VELVET</a></h5>
@@ -95,7 +95,7 @@
         <section class="home-service">
             <div class="section-heading text-center">
                 <h2 class="section-title">
-                    <div class="icon"><img src="./img/icon-DVNB.webp" alt="icon"></div>
+                    <div class="icon"><img src="../img/icon-DVNB.webp" alt="icon"></div>
                     <span>Dịch vụ nổi bật</span>
                 </h2>
                 <div class="desc"></div>
@@ -103,22 +103,22 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-3 col-md-3 col-sm-6">
-                        <img src="./img/service_img_1.webp" class="aligncenter dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
+                        <img src="../img/service_img_1.webp" class="aligncenter dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
                         <h4 class="special-text marine-food-h4" data-animation="">Bánh tươi mỗi ngày</h4>
                         <h4 class="new special-text wow animated fadeInLeft marine-food-h4-2" data-animation="fadeInLeft">Sản phẩm được sản xuất trong ngày với nguyên liệu chất lượng và tươi mới </h4>
                     </div>
                     <div class="col-lg-3 col-md-3 col-sm-6">
-                        <img src="./img/service_img_2.webp" class="aligncenter dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
+                        <img src="../img/service_img_2.webp" class="aligncenter dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
                         <h4 class="special-text marine-food-h4" data-animation="">Đa dạng chọn lựa </h4>
                         <h4 class="new special-text wow animated fadeInLeft marine-food-h4-2" data-animation="fadeInLeft">Thế giới phong phú của bánh kem tươi, bánh mì, pizza, bánh ngọt... </h4>
                     </div>
                     <div class="col-lg-3 col-md-3 col-sm-6">
-                        <img src="./img/service_img_3.webp" class="aligncenter dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
+                        <img src="../img/service_img_3.webp" class="aligncenter dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
                         <h4 class="special-text marine-food-h4" data-animation="">Hệ thống cửa hàng</h4>
                         <h4 class="new special-text wow animated fadeInLeft marine-food-h4-2" data-animation="fadeInLeft">Hệ thống 60 cửa hàng lớn và phủ khắp đáp ứng nhu cầu mua sắm tiện lợi</h4>
                     </div>
                     <div class="col-lg-3 col-md-3 col-sm-6">
-                        <img src="./img/service_img_4.webp" class="aligncenter dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
+                        <img src="../img/service_img_4.webp" class="aligncenter dont_scale wow animated fadeInDown" data-animation="fadeInDown" alt="">
                         <h4 class="special-text marine-food-h4" data-animation="">Dịch vụ tận tâm </h4>
                         <h4 class="new special-text wow animated fadeInLeft marine-food-h4-2" data-animation="fadeInLeft">Sự tỉ mỉ, tận tâm và chuyên nghiệp luôn là huyết mạch trong phục vụ </h4>
                     </div>
@@ -128,7 +128,7 @@
     <div class="container">
         <div class="section-heading text-center">
             <h2 class="section-title">
-                <div class="icon"><img src="./img/icon-TinTuc.webp" alt="icon"></div>
+                <div class="icon"><img src="../img/icon-TinTuc.webp" alt="icon"></div>
                 <span>Tin tức</span>
             </h2>
             <div class="desc">Nơi Fresh Garden cập nhật thông tin mới nhất vê sản phẩm, cửa hàng và ưu đãi</div>
@@ -138,7 +138,7 @@
                 <div class="article-content text-center">
                     <div class="article-image">
                         <a href="" title="Vui hè sang - Rộn ràng trải nghiệm cùng Fresh Garden">
-                            <img src="./img/tintuc1.webp" style="width: 90%; height: 90%;" alt="Vui hè sang - Rộn ràng trải nghiệm cùng Fresh Garden">
+                            <img src="../img/tintuc1.webp" style="width: 90%; height: 90%;" alt="Vui hè sang - Rộn ràng trải nghiệm cùng Fresh Garden">
                         </a>
                     </div>
                     <h3 class="article-title">
@@ -153,7 +153,7 @@
                 <div class="article-content text-center">
                     <div class="article-image">
                         <a href="" title="Vui hè sang - Rộn ràng trải nghiệm cùng Fresh Garden">
-                            <img src="./img/tintuc2.webp" style="width: 90%; height: 90%;" alt="Vui hè sang - Rộn ràng trải nghiệm cùng Fresh Garden">
+                            <img src="../img/tintuc2.webp" style="width: 90%; height: 90%;" alt="Vui hè sang - Rộn ràng trải nghiệm cùng Fresh Garden">
                         </a>
                     </div>
                     <h3 class="article-title">
@@ -168,7 +168,7 @@
                 <div class="article-content text-center">
                     <div class="article-image">
                         <a href="" title="Vui hè sang - Rộn ràng trải nghiệm cùng Fresh Garden">
-                            <img src="./img/tintuc3.webp" style="width: 90%; height: 90%;" alt="Vui hè sang - Rộn ràng trải nghiệm cùng Fresh Garden">
+                            <img src="../img/tintuc3.webp" style="width: 90%; height: 90%;" alt="Vui hè sang - Rộn ràng trải nghiệm cùng Fresh Garden">
                         </a>
                     </div>
                     <h3 class="article-title">
@@ -181,8 +181,7 @@
             </div>
         </div>
     </div>
-
-    <?php include("./layout/footer.php") ?>
+    <?php include("../layout/footer.php") ?>
 </body>
 
 </html>
